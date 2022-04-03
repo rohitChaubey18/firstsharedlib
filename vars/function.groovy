544ec63d206b57c985fb9ea5397459173c8d8ef6
rohitChaubey18 <101445276+rohitChaubey18@users.noreply.github.com>
2022-04-04 01:02:20 +0530

    Update function.groovy

--- vars/function.groovy
+++ vars/function.groovy
@@ -1,3 +1,5 @@
-def call(command){
-    sh "mvn ${command}"
+def call(){
+    checkout scm
+    config = readProperties file: "configuration"
+    git branch: "${config.git_branch}, url: "${config.git_url}"
 }
